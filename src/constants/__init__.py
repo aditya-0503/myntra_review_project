080b00fdc57f00ca9ba8e5fc82da9df429aa0dbf
aditya <aditya797244@gmail.com>
2025-04-24 12:17:18 +0530

    completed

---
+++ src/constants/__init__.py
@@ -0,0 +1,5 @@
+MONGO_URL_KEY: str ="mongodb+srv://aditya:98237899928@cluster0.9z5tzkb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_DATABASE_NAME: str = "myntra-reviews"
+
+
+SESSION_PRODUCT_KEY: str = "product_name"
